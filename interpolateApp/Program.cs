c2af8fe4ccb0026b7a271cff669575217aca5b08
Elizaveta Kapustina <elliz.kp@gmail.com>
2021-09-26 18:43:02 +0400

    Update Program.cs

--- interpolateApp/Program.cs
+++ interpolateApp/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 
+
 namespace interpolateApp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double[,] f = new double[,] { { 1, 2 }, { 3, -1 }, { 5, 6 }, { 7, 8 } };
+            Console.WriteLine("Enter value from (1,7):");
+            double z = double.Parse(Console.ReadLine());
+            try
+            {
+                double res = Interpolation.Interpolate(f, z);
+                Console.WriteLine($"f(z) = {res}");
+            }
+            catch (ArgumentException err)
+            {
+                Console.WriteLine(value: err.Message);
+            }
         }
     }
 }
